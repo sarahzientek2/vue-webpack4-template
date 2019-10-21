b9036c7e16b51040cf04e21883416d8699ade322
sarahzientek <a99742@aup.edu>
2019-10-21 13:58:16 +0000

    update

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,27 @@
         <b-col
           cols="8"
           offset="2">
-          <p> <router-link to="/"> Home </router-link> <router-link to="/about"> About </router-link> <router-link to="/contact"> Contact </router-link> <router-link to="/menu"> Menu </router-link></p>
+
+<div>
+            <b-navbar toggleable="lg" type="dark" variant="info">
+                <b-navbar-brand href="#">WATER BAR</b-navbar-brand>
+
+                <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+                <b-collapse id="nav-collapse" is-nav>
+                <b-navbar-nav>
+                    <b-link to="/" router-tag="b-nav-item">HOME</b-link>
+                    <b-link to="/about"  router-tag="b-nav-item">ABOUT</b-link>
+                    <b-link to="/menu"  router-tag="b-nav-item">MENU</b-link>
+                    <b-link to="/contact"  router-tag="b-nav-item">CONTACT</b-link>
+
+                </b-navbar-nav>
+
+                </b-collapse>
+            </b-navbar>
+            </div>
+
+
         </b-col>
       </b-row>
       <b-row>
