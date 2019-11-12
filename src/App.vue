9d5067d86635616ee1574c1b40ca3ccbab746acf
sarahzientek2 <sarahezientek@gmail.com>
2019-11-12 11:23:12 +0000

    updates

--- src/App.vue
+++ src/App.vue
@@ -7,9 +7,18 @@
           offset="0"
         >
           <div>
-             <b-navbar variant="faded" type="light">
-                <b-navbar-brand href="#">
-                <img src="https://lh3.googleusercontent.com/AuLBHHc0ePWAFPQiMg_HETdPIIzBFp7HDXbb2mABbJvo_yqCu4URywVhm-4tKvbqhtQB5C8gUYj2K7-7AACgS-RONLFtqrvLoKQVhzS2UC01B9JnlreJ-_wOWU6A7tOxrJBLNGw0KC4=w2400" height="42" width="42" class="d-inline-block align-top" alt="Chance">
+            <b-navbar
+              variant="faded"
+              type="light"
+            >
+              <b-navbar-brand href="#">
+                <img
+                  src="https://lh3.googleusercontent.com/AuLBHHc0ePWAFPQiMg_HETdPIIzBFp7HDXbb2mABbJvo_yqCu4URywVhm-4tKvbqhtQB5C8gUYj2K7-7AACgS-RONLFtqrvLoKQVhzS2UC01B9JnlreJ-_wOWU6A7tOxrJBLNGw0KC4=w2400"
+                  height="42"
+                  width="42"
+                  class="d-inline-block align-top"
+                  alt="Chance"
+                >
                 CHANCE ZIENTEK
               </b-navbar-brand>
 
@@ -20,30 +29,41 @@
                 is-nav
               >
                 <b-navbar-nav>
-                    <b-link
+                  <b-link
                     to="/"
-                    router-tag="b-nav-item">
+                    router-tag="b-nav-item"
+                  >
                     HOME
-                    </b-link>
+                  </b-link>
 
-                    <b-link
+                  <b-link
                     to="/about"
-                    router-tag="b-nav-item">
+                    router-tag="b-nav-item"
+                  >
                     ABOUT
-                    </b-link>
+                  </b-link>
 
-                    <b-link
+                  <b-link
                     to="/otherpg"
-                    router-tag="b-nav-item">
+                    router-tag="b-nav-item"
+                  >
                     PAGE1
-                    </b-link>
+                  </b-link>
 
-                    <b-link
+                  <b-link
                     to="/otherpg2"
-                    router-tag="b-nav-item">
+                    router-tag="b-nav-item"
+                  >
                     PAGE2
-                    </b-link>
+                  </b-link>
 
+                  <b-link
+                    to="/contact"
+                    router-tag="b-nav-item"
+                  >
+                    CONTACT
+                  </b-link>
+                  
                 </b-navbar-nav>
               </b-collapse>
             </b-navbar>
