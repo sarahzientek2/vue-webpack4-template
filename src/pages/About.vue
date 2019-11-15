7b0563d41b5605e3745895b1b348af47e2e141b7
sarahzientek2 <sarahezientek@gmail.com>
2019-11-15 18:22:06 +0000

    update

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -1,7 +1,11 @@
 <template>
-  <div class="full-width center-content">
+  <div class="container">
     <div>
-      <b-tabs content-class="mt-3">
+    <h1> Who is Chance?</h1>
+    </div>
+    
+    <div>
+      <b-tabs class="tab">
         <b-tab
           title="First"
           active
@@ -11,17 +15,33 @@
         <b-tab title="Second">
           <p>I'm the second tab</p>
         </b-tab>
-        <b-tab
-          title="Disabled"
-          disabled
-        >
-          <p>I'm a disabled tab!</p>
-        </b-tab>
+       
       </b-tabs>
     </div>
+ 
+ 
   </div>
 </template>
 
 <script>
 
 </script>
+
+<style>
+
+.container {
+    padding-top: 50px;
+  width: 100%;
+  margin: 0 auto;
+  max-width: 700px;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  top: 0;
+}
+
+p {
+    padding-top: 10px;
+}
+
+</style>
