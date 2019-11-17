ef199fdb93cef06bf5121f2e74636196c64e6fe0
sarahzientek2 <sarahezientek@gmail.com>
2019-11-17 16:34:36 +0000

    update

--- src/pages/specialtalents.vue
+++ src/pages/specialtalents.vue
@@ -3,62 +3,58 @@
     <ul class="list-unstyled">
       <b-media tag="li">
         <template v-slot:aside>
-          <b-img src="https://lh3.googleusercontent.com/98rSBy7zzVQ-NTFx0LBVp6pSzF8IYo6qM0A7qKGLbFu2nbwXy7hqgJcfp26mJNOJC_7L_pH9X6_dPWGuAvAl4TBHIILj33cXmXoNIS-4c_qjWIJLsuGxwhECYVdRLgKvnS2f85CcBsY=w2400"
+          <b-img
+            src="https://lh3.googleusercontent.com/98rSBy7zzVQ-NTFx0LBVp6pSzF8IYo6qM0A7qKGLbFu2nbwXy7hqgJcfp26mJNOJC_7L_pH9X6_dPWGuAvAl4TBHIILj33cXmXoNIS-4c_qjWIJLsuGxwhECYVdRLgKvnS2f85CcBsY=w2400"
             img-alt="Image"
-            width="64">
-            </b-img>
+            width="64"
+          />
         </template>
         <h5 class="mt-0 mb-1">
           Climbing
         </h5>
-        <p class="mb-0">
-         
-        </p>
+        <p class="mb-0" />
       </b-media>
 
       <b-media tag="li">
         <template v-slot:aside>
-          <b-img src="https://lh3.googleusercontent.com/ugn2dZv2-tLss4J28tfF9oOoE7qfre-7Fb1ZdkMUCqzbWWIOZa2B4gu3Q0Jy0RavqSFJ7L_V4_azvaQzYsvzu8V6njpXGUsgZRKVrC5OnxC2Rz4oCFxnaRgKT_LA14Nwb3dJ1Gj2vcg=w2400"
+          <b-img
+            src="https://lh3.googleusercontent.com/ugn2dZv2-tLss4J28tfF9oOoE7qfre-7Fb1ZdkMUCqzbWWIOZa2B4gu3Q0Jy0RavqSFJ7L_V4_azvaQzYsvzu8V6njpXGUsgZRKVrC5OnxC2Rz4oCFxnaRgKT_LA14Nwb3dJ1Gj2vcg=w2400"
             img-alt="Image"
-            width="64">
-            </b-img>
+            width="64"
+          />
         </template>
         <h5 class="mt-0 mb-1">
-          Jumping 
-          </h5>
-        <p class="mb-0">
-          
-        </p>
+          Jumping
+        </h5>
+        <p class="mb-0" />
       </b-media>
 
       <b-media tag="li">
         <template v-slot:aside>
-          <b-img src="https://lh3.googleusercontent.com/xq-XvbBovS9XcwwvTAFcbu6TWoZIepubgC9R_pNklrIx7W-Ek5taYGxMZi92iy0aT2zN9jlPsHt2BBUN3bHpb4KXgr61mClM-rZdww2aPpNREmLI7RUMBmnbAE5iapB_JAU-CUjU544=w2400"
+          <b-img
+            src="https://lh3.googleusercontent.com/xq-XvbBovS9XcwwvTAFcbu6TWoZIepubgC9R_pNklrIx7W-Ek5taYGxMZi92iy0aT2zN9jlPsHt2BBUN3bHpb4KXgr61mClM-rZdww2aPpNREmLI7RUMBmnbAE5iapB_JAU-CUjU544=w2400"
             img-alt="Image"
-            width="64">
-            </b-img>
+            width="64"
+          />
         </template>
         <h5 class="mt-0 mb-1">
           High Fiving
         </h5>
-        <p class="mb-0">
-         
-        </p>
+        <p class="mb-0" />
       </b-media>
 
       <b-media tag="li">
         <template v-slot:aside>
-          <b-img src="https://lh3.googleusercontent.com/BSonr9toMIwEfhTXa3PbVQ_osR44UxSI8X86D2iyJ9nj5up_wwluWUnEf4aDt6bnZX3hYAFpwUj9uhUasyFfzM6xkhfVY-8GvCXq4t4cjy1g8v1yS9aj4i_Ax5IRfJczLv4mZUnCIgw=w2400"
+          <b-img
+            src="https://lh3.googleusercontent.com/BSonr9toMIwEfhTXa3PbVQ_osR44UxSI8X86D2iyJ9nj5up_wwluWUnEf4aDt6bnZX3hYAFpwUj9uhUasyFfzM6xkhfVY-8GvCXq4t4cjy1g8v1yS9aj4i_Ax5IRfJczLv4mZUnCIgw=w2400"
             img-alt="Image"
-            width="64">
-            </b-img>
+            width="64"
+          />
         </template>
         <h5 class="mt-0 mb-1">
           Sitting
         </h5>
-        <p class="mb-0">
-          
-        </p>
+        <p class="mb-0" />
       </b-media>
 
       <b-media
@@ -67,19 +63,16 @@
       >
         <template v-slot:aside>
           <b-img
-            blank
-            blank-color="#cba"
+            src="https://lh3.googleusercontent.com/s78U-B3AWyiMuuRcfIgcudZCElX6F3IcXhZ1tL6IdqKbxwRaOLo-wEsAjhMprz8hc-ipmCKCgJAIIhp8cvS9lBmdHW9RGEA9Rkw9b2Yi-rOEnOEwNBdQc7nSvPEQVlKfk7ha4Yl_msk=w2400"
+            img-alt="Image"
             width="64"
-            alt="placeholder"
           />
         </template>
 
         <h5 class="mt-0 mb-1">
           Rolling Over
         </h5>
-        <p class="mb-0">
-          
-        </p>
+        <p class="mb-0" />
       </b-media>
     </ul>
   </div>
